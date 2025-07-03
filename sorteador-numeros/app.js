1c3f566f57f001d5e924dc95e4bba558dce8d7f2
Giordano Mpc <giordano.2002@gmail.com>
2025-07-03 13:06:37 -0300

    trabalhando na função sortear()

--- sorteador-numeros/app.js
+++ sorteador-numeros/app.js
@@ -1,14 +1,22 @@
 function sortear(){
-    //let quantNumerosSorteados = document.getElementById('quantidade').value;
+    let quantNumerosSorteados = document.getElementById('quantidade').value;
     let numeroLimiteInicio = document.getElementById('de').value;
     let numeroLimiteFinal = document.getElementById('ate').value;
     let listaSorteio = [];
     let numeroSorteado = 0;
+    let i = 0;
 
     numeroSorteado = parseInt(Math.random() * numeroLimiteFinal + 1);
-    if(numeroSorteado )
+    listaSorteio = [1,2,3,4,5];
+    while(i = 1, i < listaSorteio.length(), i++ ){
+        if(numeroSorteado < numeroLimiteInicio){
+        numeroSorteado = parseInt(Math.random() * numeroLimiteFinal + 1);
+    }else{
+        listaSorteio.push(numeroSorteado);
+    }
     
-    listaSorteio.push(numeroSorteado);
+    }
+
     
         
 }
