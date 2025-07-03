e428125dbb5a03c229a4cce1011385d8f164f6b5
Giordano Mpc <giordano.2002@gmail.com>
2025-07-03 13:06:08 -0300

    vc

---
+++ sorteador-numeros/app.js
@@ -0,0 +1,18 @@
+function sortear(){
+    //let quantNumerosSorteados = document.getElementById('quantidade').value;
+    let numeroLimiteInicio = document.getElementById('de').value;
+    let numeroLimiteFinal = document.getElementById('ate').value;
+    let listaSorteio = [];
+    let numeroSorteado = 0;
+
+    numeroSorteado = parseInt(Math.random() * numeroLimiteFinal + 1);
+    if(numeroSorteado )
+    
+    listaSorteio.push(numeroSorteado);
+    
+        
+}
+
+function reiniciar(){
+
+}
