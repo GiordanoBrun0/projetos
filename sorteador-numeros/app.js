69f24f2ea896f75091e51b8505ebd724f52ae224
Giordano Mpc <giordano.2002@gmail.com>
2025-07-11 14:39:27 -0300

    colocando comentarios no sorteado de números

--- sorteador-numeros/app.js
+++ sorteador-numeros/app.js
@@ -1,43 +1,59 @@
 function reiniciar(){
+    //reiniciando o sistema
     alterarStatusBotao();
     limparCampos();
 }
 
 function sortear(){
+    //pegando os valores pelo id do HTML 
+    // e colocando em variaveis 
     let quantNumerosSorteados = parseInt(document.getElementById('quantidade').value);
     let numeroLimiteInicio = parseInt(document.getElementById('de').value);
     let numeroLimiteFinal = parseInt(document.getElementById('ate').value);
     let listaSorteio = [];
     let numero;
 
+    //proteção para o usuario não colocar 
+    //valor de inicio maior que valor final
     if (numeroLimiteInicio >= numeroLimiteFinal) {
         alert('Campo "Do número" deve ser inferior ao campo "Até o número". Verifique!');
         limparCampos();
         return;
     }
 
+    //proteção para usuario não colocar uma quantidade 
+    //maior de diferença entre inicio e fim
     if (quantNumerosSorteados > (numeroLimiteFinal - numeroLimiteInicio + 1)) {
         alert('Campo "Quantidade" deve ser menor ou igual ao intervalo informado no campo "Do número" até o campo "Até o número". Verifique!');
         limparCampos();
         return;
     }
-
+    //definindo os números escolhidos 
     for (let i = 0; i < quantNumerosSorteados; i++){
         numero = obterNumeroAleatorio(numeroLimiteInicio, numeroLimiteFinal);
+        
+        //verifica se o número sorteado já tá na lista 
+        // para incluir um novo não escolhido
         while(listaSorteio.includes(numero)){
             numero = obterNumeroAleatorio(numeroLimiteInicio, numeroLimiteFinal);
         }
         listaSorteio.push(numero);
     }
+    //verifica pelo id do HTML onde mostrar resultado
     let resultado = document.getElementById('resultado');
-    resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados: ${listaSorteio}</label>`
-    console.log(`Lista sorteada: ${listaSorteio}`);
 
+    //altera ele para mostrar a lista com os números sorteados. 
+    resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados: ${listaSorteio}</label>`
+
+    //altera o botão reiniciar 
+    // para que ele dê a possibilidade para o usuario
     alterarStatusBotao();
 }
 
 
 function limparCampos(){
+    //Pegando os valores incluidos no seu ID 
+    // e substituindo para vazio
     document.getElementById('quantidade').value = '';
     document.getElementById('de').value = '';
     document.getElementById('ate').value = '';
@@ -45,6 +61,8 @@ function limparCampos(){
 }
 
 function alterarStatusBotao(){
+    //a partir do id conferimos o valor do botão 
+    // e substituimos para seu contrario
     let botao = document.getElementById('btn-reiniciar');
     if(botao.classList.contains('container__botao-desabilitado')){
         botao.classList.remove('container__botao-desabilitado');
@@ -56,5 +74,6 @@ function alterarStatusBotao(){
 }
 
 function obterNumeroAleatorio(min, max){
+    //procura um número aleatório conforme seu valor minimo e máximo
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
