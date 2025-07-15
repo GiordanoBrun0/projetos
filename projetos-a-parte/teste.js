4c181851fa1f43941b75a49ac9ac2a09d4429779
Giordano Mpc <giordano.2002@gmail.com>
2025-07-15 18:02:47 -0300

    ultimo teste

--- projetos-a-parte/teste.js
+++ projetos-a-parte/teste.js
@@ -247,7 +247,53 @@ function procurandoNaLista(elemento, lista) {
     }
 }
 
+function calcularSomaProduto(array) {
+    let somaPares = 0;
+    let produtoImpares = 1;
+
+    for (let numero of array) {
+        if (numero % 2 === 0) {
+            somaPares += numero;
+        } else {
+            produtoImpares *= numero;
+        }
+    }
+
+    return {
+        somaPares,
+        produtoImpares
+    };
+}
+// Função que verifica se um objeto está presente no array
+function verificarObjetoNoArray(arr, objeto) {
+    return arr.some(item => JSON.stringify(item) === JSON.stringify(objeto));
+}
 /*
+//teste  da função verificar objeto no array
+const alunos = [
+    { id: 1, nome: 'João', idade: 20 },
+    { id: 2, nome: 'Maria', idade: 22 },
+    { id: 3, nome: 'Pedro', idade: 21 },
+    { id: 4, nome: 'Ana', idade: 19 }
+];
+
+const alunoProcurado = { id: 2, nome: 'Maria', idade: 22 };
+
+const objetoEstaPresente = verificarObjetoNoArray(alunos, alunoProcurado);
+
+if (objetoEstaPresente) {
+    console.log('O aluno está presente no array.');
+} else {
+    console.log('O aluno não está presente no array.');
+}
+
+
+//teste da função calcular soma produto
+const numeros = [1, 2, 3, 4, 5];
+const resultado = calcularSomaProduto(numeros);
+console.log("Soma dos pares:", resultado.somaPares);
+console.log("Produto dos ímpares:", resultado.produtoImpares);
+
 // Teste função de procurando na lista e seu tamanho
 let minhaList = [1, 2, 3, 4, 5];
 let procura = 4;
